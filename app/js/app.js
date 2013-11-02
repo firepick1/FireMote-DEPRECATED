444f8ed3dbb5563ac1e05c6f7e726c487ad5a88e
Karl Lew <karl@firepick.org>
2013-11-01 21:57:38 -0700

    pix

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,15 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', [
+angular.module('FireMote', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
+  'FireMote.filters',
+  'FireMote.services',
+  'FireMote.directives',
+  'FireMote.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/calibrate.html', controller: 'CalibrateCtrl'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,37 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('FireMote.controllers', []).
+  controller('MyCtrl1', [function($scope) {
+
+		$scope.foo = "FOO1";
+
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('MyCtrl2', [function($scope) {
 
   }]);
+
+function CalibrateCtrl($scope, $location) {
+}
+
+function MainCtrl($scope, $location) {
+		$scope.foo = "FOOMAIN";
+		$scope.imageLarge = false;
+		$scope.headPos = 100;
+		$scope.headPosMax = 625;
+		$scope.sliderW = 44;
+
+		$scope.camImageHeight = function() {
+			return $scope.imageLarge ? "100%" : "65px";
+		}
+
+		$scope.camImageClick = function() {
+			$scope.imageLarge = !$scope.imageLarge;
+		}
+
+		$scope.go = function(hash) {
+			$location.path(hash);
+		}
+}
+
