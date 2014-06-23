dac6cc3cd3ea99868aa85d52492f37dce61c2d36
firepick1 (pixel) <karl@firepick.org>
2014-06-23 13:25:35 -0700

    ...

--- common/ts/FireStep.ts
+++ common/ts/FireStep.ts
@@ -1,71 +1,71 @@
 ///<reference path='../../include.d.ts'/>
 module firemote {
-	export class FireStep {
-			vel: number = 0;	// [vel]  velocity           - actual velocity - may be different than programmed feed rate 
-		  feed: number = 0; // [feed] feed_rate          - gcode programmed feed rate (F word) 
-			stat: number = 0; // [stat] machine_state      - 0=initializing, 1=ready, 2=shutdown, 3=stop, 4=end, 5=run, 6=hold, 9=homing 
-			//unit: number = 0; // [unit] units_mode         - 0=inch, 1=mm
-		  coor: number = 0; // [coor] coordinate_system  - 0=g53, 1=g54, 2=g55, 3=g56, 4=g57, 5=g58, 6=g59
-			momo: number = 0; // [momo] motion_mode        - 0=traverse, 1=straight feed, 2=cw arc, 3=ccw arc
-			//plan: number = 0; // [plan] plane_select       - 0=XY plane, 1=XZ plane, 2=YZ plane
-			path: number = 0; // [path] path_control_mode  - 0=exact stop, 1=exact path, 2=continuous
-			dist: number = 0; // [dist] distance_mode      - 0=absolute distance, 1=incremental distance
-			//frmo: number = 0; // [frmo] feed_rate_mode     - 0=units-per-minute-mode, 1=inverse-time-mode
-			hold: number = 0; // [hold] hold_state         - 0=off, 1=sync, 2=plan, 3=decel, 4=holding, 5=end-hold
+  export class FireStep {
+      vel: number = 0;  // [vel]  velocity           - actual velocity - may be different than programmed feed rate 
+      feed: number = 0; // [feed] feed_rate          - gcode programmed feed rate (F word) 
+      stat: number = 0; // [stat] machine_state      - 0=initializing, 1=ready, 2=shutdown, 3=stop, 4=end, 5=run, 6=hold, 9=homing 
+      //unit: number = 0; // [unit] units_mode         - 0=inch, 1=mm
+      coor: number = 0; // [coor] coordinate_system  - 0=g53, 1=g54, 2=g55, 3=g56, 4=g57, 5=g58, 6=g59
+      momo: number = 0; // [momo] motion_mode        - 0=traverse, 1=straight feed, 2=cw arc, 3=ccw arc
+      //plan: number = 0; // [plan] plane_select       - 0=XY plane, 1=XZ plane, 2=YZ plane
+      path: number = 0; // [path] path_control_mode  - 0=exact stop, 1=exact path, 2=continuous
+      dist: number = 0; // [dist] distance_mode      - 0=absolute distance, 1=incremental distance
+      //frmo: number = 0; // [frmo] feed_rate_mode     - 0=units-per-minute-mode, 1=inverse-time-mode
+      hold: number = 0; // [hold] hold_state         - 0=off, 1=sync, 2=plan, 3=decel, 4=holding, 5=end-hold
 
-			posx: number = 0; // [posx] x work position    - X work position in prevailing units (mm or inch) 
-			posy: number = 0; // [posy] y work position        Reports position in current coordinate system
-			posz: number = 0; // [posz] z work position        and with any G92 offsets in effect
-			posa: number = 0; // [posa] a work position
-			//posb: number = 0; // [posb] b work position
-			//posc: number = 0; // [posc] c work position
+      posx: number = 0; // [posx] x work position    - X work position in prevailing units (mm or inch) 
+      posy: number = 0; // [posy] y work position        Reports position in current coordinate system
+      posz: number = 0; // [posz] z work position        and with any G92 offsets in effect
+      posa: number = 0; // [posa] a work position
+      //posb: number = 0; // [posb] b work position
+      //posc: number = 0; // [posc] c work position
 
-			mpox: number = 0; // [mpox] x machine position - X machine position in absolute coordinates in mm units ONLY
-			mpoy: number = 0; // [mpoy] y machine position     Reports the internal canonical position which is 
-			mpoz: number = 0; // [mpoz] z machine position     always in mm and absolute machine coordinates (G53)
-			mpoa: number = 0; // [mpoa] a machine position     Note that no coordinate systems or g92 offsets are applied
-			//mpob: number = 0; // [mpob] b machine position
-			//mpoc: number = 0; // [mpoc] c machine position
+      mpox: number = 0; // [mpox] x machine position - X machine position in absolute coordinates in mm units ONLY
+      mpoy: number = 0; // [mpoy] y machine position     Reports the internal canonical position which is 
+      mpoz: number = 0; // [mpoz] z machine position     always in mm and absolute machine coordinates (G53)
+      mpoa: number = 0; // [mpoa] a machine position     Note that no coordinate systems or g92 offsets are applied
+      //mpob: number = 0; // [mpob] b machine position
+      //mpoc: number = 0; // [mpoc] c machine position
 
-			//ofsx: number = 0; // [ofsx] x machine offset   - X machine offset to work position in absolute coordinates in mm units ONLY
-			//ofsy: number = 0; // [ofsy] y machine offset       Reports offsets in mm and absolute machine coordinates 
-			//ofsz: number = 0; // [ofsz] z machine offset
-			//ofsa: number = 0; // [ofsa] a machine offset
-			//ofsb: number = 0; // [ofsb] b machine offset
-			//ofsc: number = 0; // [ofsc] c machine offset
+      //ofsx: number = 0; // [ofsx] x machine offset   - X machine offset to work position in absolute coordinates in mm units ONLY
+      //ofsy: number = 0; // [ofsy] y machine offset       Reports offsets in mm and absolute machine coordinates 
+      //ofsz: number = 0; // [ofsz] z machine offset
+      //ofsa: number = 0; // [ofsa] a machine offset
+      //ofsb: number = 0; // [ofsb] b machine offset
+      //ofsc: number = 0; // [ofsc] c machine offset
 
-			//g54x: number = 0; // [g54x] g54x               - G54 coordinate system offset for X axis
-			//g54y: number = 0; // [g54y] g54y                   Coordinate system offsets may be reported
-			//g54z: number = 0; // [g54z] g54z
-			//...
-			//g59a: number = 0; // [g59a] g59a
-			//g59b: number = 0; // [g59b] g59b
-			//g59c: number = 0; // [g59c] g59c
+      //g54x: number = 0; // [g54x] g54x               - G54 coordinate system offset for X axis
+      //g54y: number = 0; // [g54y] g54y                   Coordinate system offsets may be reported
+      //g54z: number = 0; // [g54z] g54z
+      //...
+      //g59a: number = 0; // [g59a] g59a
+      //g59b: number = 0; // [g59b] g59b
+      //g59c: number = 0; // [g59c] g59c
 
-			//g92x: number = 0; // [g92x] g92x               - G92 origin offset for X axis
-			//g92y: number = 0; // [g92y] g92y                   The number returned can be a bit brain bending as you have to back out 
-			//g92z: number = 0; // [g92z] g92z                   the position from which the G92 was set, but this is the actual offset 
-			//g29a: number = 0; // [g29a] g92a                   value; and may be different from the value entered in the G92 command.
-			//g92b: number = 0; // [g92b] g92b
-			//g92c: number = 0; // [g92c] g92c
+      //g92x: number = 0; // [g92x] g92x               - G92 origin offset for X axis
+      //g92y: number = 0; // [g92y] g92y                   The number returned can be a bit brain bending as you have to back out 
+      //g92z: number = 0; // [g92z] g92z                   the position from which the G92 was set, but this is the actual offset 
+      //g29a: number = 0; // [g29a] g92a                   value; and may be different from the value entered in the G92 command.
+      //g92b: number = 0; // [g92b] g92b
+      //g92c: number = 0; // [g92c] g92c
 
-			constructor(obj = undefined) {
-				if (typeof obj === 'string') {
-				  obj = JSON.parse(obj);
-				}
-				if (typeof obj !== 'undefined') {
-				  for (var k in this) {
-					   if (typeof this[k] === 'number') {
-						   this[k] = obj[k];
-						 }
-					}
-				}
-			}
+      constructor(obj = undefined) {
+        if (typeof obj === 'string') {
+          obj = JSON.parse(obj);
+        }
+        if (typeof obj !== 'undefined') {
+          for (var k in this) {
+             if (typeof this[k] === 'number') {
+               this[k] = obj[k];
+             }
+          }
+        }
+      }
 
-			clone(): FireStep {
-				return new FireStep(this);
-			}
-	}
+      clone(): FireStep {
+        return new FireStep(this);
+      }
+  }
 }
 
 exports.FireStep = firemote.FireStep;
--- common/ts/MachineState.ts
+++ common/ts/MachineState.ts
@@ -1,128 +1,128 @@
 ///<reference path='../../include.d.ts'/>
 module firemote {
-	export class MachineState {
-		message: string = "FirePick machine state";
-		stateId: number = 1;
-		logLevel: string = "INFO";
-		firefuse: Boolean = true;
-		gcode: string = "";
-		gantries: Gantry[] = [];
-		trayFeeders: TrayFeeder[] = [];
-		pcbFeeders: PcbFeeder[] = [];
-		firestep: FireStep = new FireStep();
+  export class MachineState {
+    message: string = "FirePick machine state";
+    stateId: number = 1;
+    logLevel: string = "INFO";
+    firefuse: Boolean = true;
+    gcode: string = "";
+    gantries: Gantry[] = [];
+    trayFeeders: TrayFeeder[] = [];
+    pcbFeeders: PcbFeeder[] = [];
+    firestep: FireStep = new FireStep();
 
-		constructor(obj = undefined) {
-			if (typeof obj === 'string') {
-			  obj = JSON.parse(obj);
-			}
-			if (typeof obj !== 'undefined') {
-			  if (obj.hasOwnProperty("message")) this.message = obj.message;
-			  if (obj.hasOwnProperty("gcode")) this.gcode = obj.gcode;
-			  if (obj.hasOwnProperty("stateId")) this.stateId = obj.stateId;
-			  if (obj.hasOwnProperty("logLevel")) this.logLevel = obj.logLevel;
-			  if (obj.hasOwnProperty("firefuse")) this.firefuse = obj.firefuse;
-			  if (obj.hasOwnProperty("firestep")) this.firestep = obj.firestep;
-				if (obj.gantries && obj.gantries.length > 0) {
-				  for (var i = 0; i < obj.gantries.length; i++) {
-					  this.gantries.push(new Gantry(obj.gantries[i]));
-					}
-				}
-				if (obj.trayFeeders && obj.trayFeeders.length > 0) {
-				  for (var i = 0; i < obj.trayFeeders.length; i++) {
-					  this.trayFeeders.push(new TrayFeeder(obj.trayFeeders[i]));
-					}
-				}
-				if (obj.pcbFeeders && obj.pcbFeeders.length > 0) {
-				  for (var i = 0; i < obj.pcbFeeders.length; i++) {
-					  this.pcbFeeders.push(new PcbFeeder(obj.pcbFeeders[i]));
-					}
-				}
-			}
-			this.gantries.length > 0 || this.gantries.push(new Gantry({axis:{name:"Gantry", gcAxis:"y"}}));
-			this.trayFeeders.length > 0 || this.trayFeeders.push(new TrayFeeder({axis:{name:"Tray Feeder", gcAxis:"x"}}));
-			this.pcbFeeders.length > 0 || this.pcbFeeders.push(new PcbFeeder({axis:{name:"PCB Feeder", gcAxis:"z"}}));
-		}
+    constructor(obj = undefined) {
+      if (typeof obj === 'string') {
+        obj = JSON.parse(obj);
+      }
+      if (typeof obj !== 'undefined') {
+        if (obj.hasOwnProperty("message")) this.message = obj.message;
+        if (obj.hasOwnProperty("gcode")) this.gcode = obj.gcode;
+        if (obj.hasOwnProperty("stateId")) this.stateId = obj.stateId;
+        if (obj.hasOwnProperty("logLevel")) this.logLevel = obj.logLevel;
+        if (obj.hasOwnProperty("firefuse")) this.firefuse = obj.firefuse;
+        if (obj.hasOwnProperty("firestep")) this.firestep = obj.firestep;
+        if (obj.gantries && obj.gantries.length > 0) {
+          for (var i = 0; i < obj.gantries.length; i++) {
+            this.gantries.push(new Gantry(obj.gantries[i]));
+          }
+        }
+        if (obj.trayFeeders && obj.trayFeeders.length > 0) {
+          for (var i = 0; i < obj.trayFeeders.length; i++) {
+            this.trayFeeders.push(new TrayFeeder(obj.trayFeeders[i]));
+          }
+        }
+        if (obj.pcbFeeders && obj.pcbFeeders.length > 0) {
+          for (var i = 0; i < obj.pcbFeeders.length; i++) {
+            this.pcbFeeders.push(new PcbFeeder(obj.pcbFeeders[i]));
+          }
+        }
+      }
+      this.gantries.length > 0 || this.gantries.push(new Gantry({axis:{name:"Gantry", gcAxis:"y"}}));
+      this.trayFeeders.length > 0 || this.trayFeeders.push(new TrayFeeder({axis:{name:"Tray Feeder", gcAxis:"x"}}));
+      this.pcbFeeders.length > 0 || this.pcbFeeders.push(new PcbFeeder({axis:{name:"PCB Feeder", gcAxis:"z"}}));
+    }
 
-		validate(): MachineState {
-	    for (var i = 0; i < this.gantries.length; i++) {
-			  this.gantries[i].validate();
-			}
-	    for (var i = 0; i < this.trayFeeders.length; i++) {
-			  this.trayFeeders[i].validate();
-			}
-	    for (var i = 0; i < this.pcbFeeders.length; i++) {
-			  this.pcbFeeders[i].validate();
-			}
-		  return this;
-		}
+    validate(): MachineState {
+      for (var i = 0; i < this.gantries.length; i++) {
+        this.gantries[i].validate();
+      }
+      for (var i = 0; i < this.trayFeeders.length; i++) {
+        this.trayFeeders[i].validate();
+      }
+      for (var i = 0; i < this.pcbFeeders.length; i++) {
+        this.pcbFeeders[i].validate();
+      }
+      return this;
+    }
 
-		clearForLinearMotion() {
-		  for (var i = 0; i < this.gantries.length; i++) {
-			  var spindles = this.gantries[i].head.spindles;
-			  for (var j = 0; j < spindles.length; j++) {
-					spindles[j].pos = 100; // raise all spindles
-				}
-			}
-		}
+    clearForLinearMotion() {
+      for (var i = 0; i < this.gantries.length; i++) {
+        var spindles = this.gantries[i].head.spindles;
+        for (var j = 0; j < spindles.length; j++) {
+          spindles[j].pos = 100; // raise all spindles
+        }
+      }
+    }
 
-		calibrate(gc:GCoder = new GCoder()) {
-			var axes = this.linearAxes();
-			var coords = new Coordinates();
+    calibrate(gc:GCoder = new GCoder()) {
+      var axes = this.linearAxes();
+      var coords = new Coordinates();
 
-			for (var i = 0; i < axes.length; i++) {
-				if (axes[i].calibrate === "home") {
-					coords[axes[i].gcAxis] = 0;
-					axes[i].pos = 0;
-				}
-				axes[i].calibrate = "";
-			}
+      for (var i = 0; i < axes.length; i++) {
+        if (axes[i].calibrate === "home") {
+          coords[axes[i].gcAxis] = 0;
+          axes[i].pos = 0;
+        }
+        axes[i].calibrate = "";
+      }
 
-			gc.home(coords);
-		}
+      gc.home(coords);
+    }
 
-		moveTo(newState: MachineState, gc:GCoder = new GCoder()) {
-			var axes1 = this.linearAxes();
-			var axes2 = this.linearAxes(newState);
-			var coords = new Coordinates();
+    moveTo(newState: MachineState, gc:GCoder = new GCoder()) {
+      var axes1 = this.linearAxes();
+      var axes2 = this.linearAxes(newState);
+      var coords = new Coordinates();
 
-			for (var i = 0; i < axes1.length; i++) {
-				if (axes2[i] instanceof Object) {
-					var newPos = axes2[i].pos;
-					if (!isNaN(newPos) && axes1[i].pos !== newPos) {
-						coords[axes1[i].gcAxis] = newPos;
-						axes1[i].pos = newPos;
-					}
-				}
-			}
+      for (var i = 0; i < axes1.length; i++) {
+        if (axes2[i] instanceof Object) {
+          var newPos = axes2[i].pos;
+          if (!isNaN(newPos) && axes1[i].pos !== newPos) {
+            coords[axes1[i].gcAxis] = newPos;
+            axes1[i].pos = newPos;
+          }
+        }
+      }
 
-			gc.moveTo(coords);
-		}
+      gc.moveTo(coords);
+    }
 
-		linearAxes(machine:MachineState = this): LinearAxis[] {
-		  var result:LinearAxis[] = [];
+    linearAxes(machine:MachineState = this): LinearAxis[] {
+      var result:LinearAxis[] = [];
 
-			if (machine.gantries instanceof Array) {
-				for (var i = 0; i < machine.gantries.length; i++) {
-					result.push(machine.gantries[i].axis);
-				}
-			}
-			if (machine.pcbFeeders instanceof Array) {
-				for (var i = 0; i < machine.pcbFeeders.length; i++) {
-					result.push(machine.pcbFeeders[i].axis);
-				}
-			}
-			if (machine.trayFeeders instanceof Array) {
-				for (var i = 0; i < machine.trayFeeders.length; i++) {
-					result.push(machine.trayFeeders[i].axis);
-				}
-			}
+      if (machine.gantries instanceof Array) {
+        for (var i = 0; i < machine.gantries.length; i++) {
+          result.push(machine.gantries[i].axis);
+        }
+      }
+      if (machine.pcbFeeders instanceof Array) {
+        for (var i = 0; i < machine.pcbFeeders.length; i++) {
+          result.push(machine.pcbFeeders[i].axis);
+        }
+      }
+      if (machine.trayFeeders instanceof Array) {
+        for (var i = 0; i < machine.trayFeeders.length; i++) {
+          result.push(machine.trayFeeders[i].axis);
+        }
+      }
 
-			return result;
-		}
+      return result;
+    }
 
-		clone(): MachineState {
-			return new MachineState(this);
-		}
+    clone(): MachineState {
+      return new MachineState(this);
+    }
   }
 }
 
